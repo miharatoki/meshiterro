3da8d02a5bb3e90de3922f0794903737fa120236
miharatoki <kou566533@gmail.com>
2021-07-07 08:47:14 +0000

    first commit

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = '6d8d4c7f5adf1f121ccd378ffdb00fe87b7ed1efd95b9e21a1bb5b336a9eea966f7e724d605fc1c17d233ff6f09295744a08200e5cb05e422d22cb75d714d114'
